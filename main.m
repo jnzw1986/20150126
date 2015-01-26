1641c0694ac6884a46fc77e7c8cb5526d34932e4
evan <zhaowena@yonyou.com>
2015-01-26 10:02:33 +0800

    git source tree -- commit add files

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KVC&KVO
+//
+//  Created by evan on 13-12-9.
+//  Copyright (c) 2013年 evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAAppDelegate class]));
+  }
+}
