2bb1f8a2d6c381dfc2e9d08f0b2bb9699f2d59b2
evan <zhaowena@yonyou.com>
2015-01-26 14:23:37 +0800

    新建分支：：：：

---
+++ main副本.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  KVC&KVO
+//
+//  Created by evan on 13-12-9.
+//  Copyright (c) 2013年 evan. All rights reserved.
+//
+//  Copyright (c) 2013年 evan. All rights 
+
+// 添加一行
+#import <UIKit/UIKit.h>
+
+#import "WAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAAppDelegate class]));
+  }
+}
